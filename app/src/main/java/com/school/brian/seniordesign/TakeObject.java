6e37e5aa5e7fcf471b953e42d31d5c45fc9d686f
bms117 <bms117@zips.uakron.edu>
2018-03-07 15:58:50 -0500

    take object and putobject class compiles, but is not useful yet

--- app/src/main/java/com/school/brian/seniordesign/TakeObject.java
+++ app/src/main/java/com/school/brian/seniordesign/TakeObject.java
@@ -1,8 +1,57 @@
 package com.school.brian.seniordesign;
 
-/**
- * Created by brian on 3/7/18.
- */
+import android.app.Activity;
+import android.util.Log;
 
-public class TakeObject {
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import java.io.File;
+
+public class TakeObject extends Activity {
+
+    public void downloadData() {
+
+        // Initialize AWSMobileClient if not initialized upon the app startup.
+        // AWSMobileClient.getInstance().initialize(this).execute();
+
+        TransferUtility transferUtility =
+                TransferUtility.builder()
+                        .context(getApplicationContext())
+                        .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+                        .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentialsProvider()))
+                        .build();
+
+        TransferObserver downloadObserver =
+                transferUtility.download(
+                        "s3Folder/s3Key.txt",
+                        new File("/path/to/file/localFile.txt"));
+        downloadObserver.setTransferListener(new TransferListener() {
+
+            @Override
+            public void onStateChanged(int id, TransferState state) {
+                if (TransferState.COMPLETED == state) {
+                    // Handle a completed upload.
+                }
+            }
+
+            @Override
+            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+                float percentDonef = ((float)bytesCurrent/(float)bytesTotal) * 100;
+                int percentDone = (int)percentDonef;
+
+                Log.d("MainActivity", "   ID:" + id + "   bytesCurrent: " + bytesCurrent + "   bytesTotal: " + bytesTotal + " " + percentDone + "%");
+            }
+
+            @Override
+            public void onError(int id, Exception ex) {
+                // Handle errors
+            }
+
+        });
+    }
 }
